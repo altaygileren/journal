eed49f81577d0d8f0d140754235655c07ecbe471
altaygileren <altaygileren@gmail.com>
2019-01-30 17:48:48 -0500

    trying again to fix deployment issue

--- server.js
+++ server.js
@@ -31,12 +31,13 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/posts', posts)
 
+
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 
